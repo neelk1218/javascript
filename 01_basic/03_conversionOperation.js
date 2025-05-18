1496e80e47677016b5862e9f00617adb1081861b
neelk1218 <nkhamkar2006@gmail.com>
2025-05-18 18:51:40 +0000

    conversions

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+//console.log(typeof score)
+
+
+let valueInNumber = Number(score)  
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False =>0
+// "" => false
+// "neel" =>true 
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);

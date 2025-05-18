5f369b48a933e0741275623339144abf63de6c61
neelk1218 <nkhamkar2006@gmail.com>
2025-05-18 18:29:00 +0000

    datatypes notes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+//* code readiblity should be high 
+
+let name = "neel"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//*//DATA TYPES//*//
+// number => 2 to power 53
+// bigint (big websites)
+// string =>"
+// boolean =>true/false
+// null => standalone value(empty)
+// undefined =>
+// symbol =>uniqueness
+
+
+
+//*//OBJECTS
+
+console.log(typeof state)
